fb59c6987157cbc69239925b62b9d60eedc3c0da
pabloaav <pablovicentin84@gmail.com>
2020-11-10 20:00:45 -0300

    haciendo las cosas que tenemos que hacer...

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,19 +34,22 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
+      <DatatableComponent />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
+import DatatableComponent from "./components/DatatableComponent";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
+    DatatableComponent,
   },
 
   data: () => ({
