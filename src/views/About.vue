ff8c4495015a463d9bcb1d4f8b6d355894d3fe62
pabloaav <pablovicentin84@gmail.com>
2020-11-21 16:11:23 -0300

    mas cambios

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h1>Sobre esta app...</h1>
     <p class="text-justify text--primary">
       No dejes de hacer cosas porque te olvidaste. Esta app te ayudará a
-      mantenerte siempre atento a tus tareas mas impoprtantes.
+      mantenerte siempre atento a tus tareas mas importantes.
     </p>
   </div>
 </template>
